b11b5554468b3e21542d01743694b794a22aab5a
Dmitry Klionsky <dm.klionsky@gmail.com>
2019-02-04 15:17:11 +0300

    Add example

--- ch4/DijkstraSP.java
+++ ch4/DijkstraSP.java
@@ -10,6 +10,16 @@ import edu.princeton.cs.algs4.*;
 // 0 to 6 (1.51): 0->2 0.26000 2->7 0.34000 7->3 0.39000 3->6 0.52000
 // 0 to 7 (0.60): 0->2 0.26000 2->7 0.34000
 
+// $ make run CLASS=DijkstraSP ARGS="../data/tinyEWG.txt 0"
+// 0 to 0 (0.00):
+// 0 to 1 (Infinity):
+// 0 to 2 (0.26): 0->2 0.26000
+// 0 to 3 (0.43): 0->2 0.26000 2->3 0.17000
+// 0 to 4 (0.38): 0->4 0.38000
+// 0 to 5 (0.73): 0->4 0.38000 4->5 0.35000
+// 0 to 6 (0.95): 0->2 0.26000 2->3 0.17000 3->6 0.52000
+// 0 to 7 (0.16): 0->7 0.16000
+
 public class DijkstraSP {
     private final double INFINITY = Double.POSITIVE_INFINITY;
     private DirectedEdge[] edgeTo;
