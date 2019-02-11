84c4de32e7454761456581b9721a67c7b8380da6
Dmitry Klionsky <dm.klionsky@gmail.com>
2019-02-11 17:29:33 +0300

    Add arbitrage

---
+++ ch4/Arbitrage.java
@@ -0,0 +1,34 @@
+import edu.princeton.cs.algs4.*;
+
+// $ make run CLASS=Arbitrage < ../data/rates.txt
+// 1000.00000 CAD =  995.00000 USD
+//  995.00000 USD =  737.29500 EUR
+//  737.29500 EUR = 1007.14497 CAD
+
+public class Arbitrage {
+    public static void main(String[] args) {
+        int V = StdIn.readInt();
+        String[] name = new String[V];
+        EdgeWeightedDigraph G = new EdgeWeightedDigraph(V);
+        for (int v = 0; v < V; v++) {
+            name[v] = StdIn.readString();
+            for (int w = 0; w < V; w++) {
+                double rate = StdIn.readDouble();
+                DirectedEdge e = new DirectedEdge(v, w, -Math.log(rate));
+                G.addEdge(e);
+            }
+        }
+
+        BellmanFordSPs spt = new BellmanFordSPs(G, 0);
+        if (spt.hasNegativeCycle()) {
+            double stake = 1000.0;
+            for (DirectedEdge e : spt.negativeCycle()) {
+                StdOut.printf("%10.5f %s ", stake, name[e.from()]);
+                stake *= Math.exp(-e.weight());
+                StdOut.printf("= %10.5f %s\n", stake, name[e.to()]);
+            }
+        } else {
+            StdOut.println("No arbitrage opportunity");
+        }
+    }
+}
