fe373df0de60886d910fb1cc02eaa9b5183ab72f
Dmitry Klionsky <dm.klionsky@gmail.com>
2019-02-14 19:27:18 +0300

    Link files

--- ch4/MAP.java
+++ ch4/MAP.java
@@ -1,9 +1,1 @@
-interface MAP<Key, Value> {
-    void put(Key key, Value val);
-    Value get(Key key);
-    void delete(Key key);
-    boolean contains(Key key);
-    boolean isEmpty();
-    int size();
-    Iterable<Key> keys();
-}
+../ch3/MAP.java
--- ch4/SET.java
+++ ch4/SET.java
@@ -1,7 +1,1 @@
-interface SET<Key> extends Iterable<Key> {
-    void add(Key key);
-    void delete(Key key);
-    boolean contains(Key key);
-    boolean isEmpty();
-    int size();
-}
+../ch3/SET.java
